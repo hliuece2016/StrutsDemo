4776c5e74c1dd6a0ed5230eb07106d34b28a4f2e
hliuece2016 <hliuece2016@gmail.com>
2017-10-19 21:48:31 -0400

    A simple struts2 practice project

---
+++ src/com/action/TestAction.java
@@ -0,0 +1,23 @@
+package com.action;
+import com.opensymphony.xwork2.*;
+public class TestAction extends ActionSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String helo;
+	
+	public String getHelo() {
+		return helo;
+	}
+	
+	public void setHelo(String helo) {
+		this.helo = helo;
+	}
+	
+	public String execute() throws Exception{
+		helo = "hello, world";
+		return SUCCESS;
+	}
+}
